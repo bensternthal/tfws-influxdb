0ce66045a17bfd9d5b49525d1f466f83c8731b59
Benjamin Sternthal <ben@devpatch.com>
2016-08-27 15:13:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+TFWS: Tinkerforge WeatherStation
+=========
+Node based server that sends data from [Tinkerforge
+WeatherStation](https://www.tinkerforge.com/en/shop/kits/starter-kit-weather-station.html)
+to an influxdb.
+
+Installation
+------------
+1. npm install
+1. copy ```local.json.dist``` to local.json, fill in values if different
+1. ```npm install```
+1. ```forever run app.js```
---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var conf = require('./lib/conf');
+var tfws = require('./lib/tfws');
+var Influx = require('./lib/influx');
+
+// Start Weather Station
+tfws.start();
+process.stdin.on('data', function(data) {
+  tfws.stop();
+});
+
+// Delay 5 Seconds To Allow Weather Station To Start Prior To Starting Loop
+setTimeout(function() {tfwsLoop();}, 5000);
+
+// Loop to post data at set interval to influx
+function tfwsLoop() {
+  var tfwsData = tfws.gettfwsJSON();
+
+  Influx.writeInflux(tfwsData);
+  // Every 5 minutes send data.
+  setTimeout(tfwsLoop, 300000);
+}
