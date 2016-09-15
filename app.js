5ffac239962aaa4386af067ceb2300b3d2b36bd5
Benjamin Sternthal <ben@devpatch.com>
2016-09-15 09:02:05 -0700

    Fixed eslintrc and eslint errors.

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ var Influx = require('./lib/influx');
 // Start Weather Station
 tfws.start();
 process.stdin.on('data', function(data) {
-  tfws.stop();
+    tfws.stop();
 });
 
 // Delay 5 Seconds To Allow Weather Station To Start Prior To Starting Loop
-setTimeout(function() {tfwsLoop();}, 5000);
+setTimeout(function() {
+    tfwsLoop();
+}, 5000);
 
 // Loop to post data at set interval to influx
 function tfwsLoop() {
-  var tfwsData = tfws.gettfwsJSON();
+    var tfwsData = tfws.gettfwsJSON();
 
-  Influx.writeInflux(tfwsData);
-  // Every 5 minutes send data.
-  setTimeout(tfwsLoop, 300000);
+    Influx.writeInflux(tfwsData);
+    // Every 5 minutes send data.
+    setTimeout(tfwsLoop, 300000);
 }
