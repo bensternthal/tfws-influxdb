87c9d64d5eb4fc4f0baf4962203aa4a62aaf634a
Benjamin Sternthal <ben@devpatch.com>
2016-11-05 14:28:36 -0700

    Added Slack, Added Promises, Fixed dotfiles.

--- app.js
+++ app.js
@@ -4,22 +4,42 @@ var conf = require('./lib/conf');
 var tfws = require('./lib/tfws');
 var Influx = require('./lib/influx');
 
+var SlackBot = require('slackbots');
+
+// Create & Configure Slackbot
+var bot = new SlackBot({
+    token: conf.get('slackbot:api_token'),
+    name: 'tfws-status'
+});
+var channel = conf.get('slackbot:channel');
+var params = {icon_emoji: ':tfws:'};
+
+// Post to slack we are starting up...
+bot.postMessageToGroup(channel, 'TFWS Has Started', params);
+
+
 // Start Weather Station
 tfws.start();
 process.stdin.on('data', function(data) {
+    bot.postMessageToGroup(channel, 'TFWS Shutting Down', params);
     tfws.stop();
 });
 
-// Delay 5 Seconds To Allow Weather Station To Start Prior To Starting Loop
+
+function getData() {
+
+    tfws.gettfwsJSON().then(Influx.writeInflux).then(function() {
+        setTimeout(getData, conf.get('update_frequency'));
+    }).catch(function(e) {
+        bot.postMessageToGroup(channel,  e.message);
+        // Retry
+        setTimeout(getData, conf.get('update_frequency'));
+    });
+
+};
+
+
+//Delay 5 Seconds To Allow Weather Station To Start Prior To Starting Loop
 setTimeout(function() {
-    tfwsLoop();
+    getData();
 }, 5000);
-
-// Loop to post data at set interval to influx
-function tfwsLoop() {
-    var tfwsData = tfws.gettfwsJSON();
-
-    Influx.writeInflux(tfwsData);
-    // Every 5 minutes send data.
-    setTimeout(tfwsLoop, 300000);
-}
